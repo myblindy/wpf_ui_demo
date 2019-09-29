b9208ed8765afe3ebdad2c050ff7410214d94d03
Marius Mitea <tweety.04.01@gmail.com>
2019-09-28 22:21:33 -0400

    added pages and page animations

---
+++ uidemo/Models/MainWindowModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace uidemo.Models
+{
+    public class MainWindowModel
+    {
+        public MainWindowActionBarItem[] ActionBarItems { get; } = new[]
+        {
+            new MainWindowActionBarItem { Name = "MAP", PageName = "Map" },
+            new MainWindowActionBarItem { Name = "APPROACH", PageName = "Approach" },
+            new MainWindowActionBarItem { Name = "RUNWAYS", PageName = "Runways" },
+            new MainWindowActionBarItem { Name = "ARF", PageName = "Arf" },
+        };
+    }
+
+    public class MainWindowActionBarItem
+    {
+        public string Name { get; set; }
+        public string PageName { get; set; }
+    }
+}
